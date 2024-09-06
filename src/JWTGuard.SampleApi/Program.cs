824a616b90adf6f77a49f37e1862c5afaa3d646c
Wesley Cabus <wesley@gotsharp.be>
2024-09-06 10:51:55 +0200

    Remove OpenApi/Swagger from the sample API project

--- src/JWTGuard.SampleApi/Program.cs
+++ src/JWTGuard.SampleApi/Program.cs
@@ -9,10 +9,6 @@ const string authority = "https://demo.duendesoftware.com";
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 builder.Services.AddAuthentication()
     .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
     {
@@ -126,12 +122,6 @@ builder.Services.AddAuthorization();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 app.UseHttpsRedirection();
 
 var summaries = new[]
@@ -157,7 +147,6 @@ app.MapGet("/weatherforecast", () =>
     return forecast;
 })
 .WithName("GetWeatherForecast")
-.WithOpenApi()
 .RequireAuthorization();
 
 app.Run();
